0fe655be92e84b4cc2fc51151fac54a1c3bddc03
pinn3 <iam@lacking.education>
2019-12-16 01:55:50 +0100

    Add support for object argument

--- conversion/src/bite.v1.js
+++ conversion/src/bite.v1.js
@@ -3,25 +3,41 @@
         programId: null
     }
 
+    function sendEvent({ hitType, eventCategory, eventAction, eventLabel, eventValue }) {
+        if (!state.programId) {
+            throw new Error('BarnebysAnalytics is not initialized correctly')
+        }
+
+        const query = '/?p=' + state.programId +
+            '&k='  + hitType +
+            '&d1=' + eventCategory +
+            '&d2=' + eventAction +
+            '&d3=' + eventLabel +
+            '&d4=' + eventValue
+
+        const request = new XMLHttpRequest()
+        request.open('GET', process.env.BA_HOST + query)
+        request.send()
+    }
+
     const actions = {
         'init': (programId) => {
             state.programId = programId
         },
         'send': (hitType, eventCategory, eventAction, eventLabel, eventValue) => {
-            if (!state.programId) {
-                throw new Error('BarnebysAnalytics is not initialized correctly')
+            if (typeof hitType === 'object') {
+                const eventObject = hitType
+                sendEvent(eventObject)
+                return
             }
 
-            const query = '/?p=' + state.programId +
-                '&k='  + hitType +
-                '&d1=' + eventCategory +
-                '&d2=' + eventAction +
-                '&d3=' + eventLabel +
-                '&d4=' + eventValue
-
-            const request = new XMLHttpRequest()
-            request.open('GET', process.env.BA_HOST + query)
-            request.send()
+            sendEvent({
+                hitType,
+                eventCategory,
+                eventAction,
+                eventLabel,
+                eventValue,
+            })
         }
     }
 
