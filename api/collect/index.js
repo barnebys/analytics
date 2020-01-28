3ba46395cbdc39ad92e6138f2629791e04c2fb9c
johan <johan@baboons.se>
2020-01-28 10:41:34 +0100

    Adds prettier

--- api/collect/index.js
+++ api/collect/index.js
@@ -1,82 +1,83 @@
-const { SESSION_NAME, SESSION_MAX_AGE, SECRET, SITE_URL } = process.env
+const { SESSION_NAME, SESSION_MAX_AGE, SECRET, SITE_URL } = process.env;
 
-import {collectEvent, collectTrack} from "../../lib/collect"
-import queryParser from "../../lib/queryParser"
+import { collectEvent, collectTrack } from "../../lib/collect";
+import queryParser from "../../lib/queryParser";
 
-const session = require('micro-cookie-session')({
-    name: SESSION_NAME,
-    keys: [SECRET],
-    maxAge: SESSION_MAX_AGE * 60 * 1000,
-})
+const session = require("micro-cookie-session")({
+  name: SESSION_NAME,
+  keys: [SECRET],
+  maxAge: SESSION_MAX_AGE * 60 * 1000
+});
 
-const emptygif = require('emptygif')
-const encodeUrl = require('encodeurl')
+const emptygif = require("emptygif");
+const encodeUrl = require("encodeurl");
 
 const redirect = (response, statusCode, redirectTarget) => {
-    response.writeHead(statusCode, {
-        Location: redirectTarget
-    })
-    return response.end()
-}
+  response.writeHead(statusCode, {
+    Location: redirectTarget
+  });
+  return response.end();
+};
 
 const handleTrack = async (req, res) => {
-    const { programId, kind, affiliate, url } = queryParser(req.url)
+  const { programId, kind, affiliate, url } = queryParser(req.url);
 
-    if (!programId || !kind) {
-        console.log('Missing required `programId` and/or `kind` values')
-        if (SITE_URL) {
-            return redirect(res, 307, SITE_URL)
-        }
-
-        return res.status(204).end()
+  if (!programId || !kind) {
+    console.log("Missing required `programId` and/or `kind` values");
+    if (SITE_URL) {
+      return redirect(res, 307, SITE_URL);
     }
 
-    // Start session
-    session(req, res)
+    return res.status(204).end();
+  }
 
-    // Run tracker async
-    await collectTrack(req, res)
+  // Start session
+  session(req, res);
 
-    // Handle leads from affiliates
-    if (affiliate) {
-        req.session.kind = kind
-        req.session.programId = programId
-    }
+  // Run tracker async
+  await collectTrack(req, res);
 
-    if (url) {
-        redirect(res, 307, encodeUrl(url))
-    } else {
-        return emptygif.sendEmptyGif(req, res, {
-            'Content-Type' : 'image/gif',
-            'Content-Length' : emptygif.emptyGifBufferLength,
-            'Cache-Control' : 'public, max-age=0'
-        });
-    }
-}
+  // Handle leads from affiliates
+  if (affiliate) {
+    req.session.kind = kind;
+    req.session.programId = programId;
+  }
+
+  if (url) {
+    redirect(res, 307, encodeUrl(url));
+  } else {
+    return emptygif.sendEmptyGif(req, res, {
+      "Content-Type": "image/gif",
+      "Content-Length": emptygif.emptyGifBufferLength,
+      "Cache-Control": "public, max-age=0"
+    });
+  }
+};
 
 const handleEvent = async (req, res) => {
-    const { programId, action, category } = queryParser(req.url)
+  const { programId, action, category } = queryParser(req.url);
 
-    if (!programId || !action || !category) {
-        console.log('Missing required `programId` and/or `action` and/or `category` values')
-    } else {
-        await collectEvent(req, res)
-    }
+  if (!programId || !action || !category) {
+    console.log(
+      "Missing required `programId` and/or `action` and/or `category` values"
+    );
+  } else {
+    await collectEvent(req, res);
+  }
 
-    return emptygif.sendEmptyGif(req, res, {
-        'Content-Type' : 'image/gif',
-        'Content-Length' : emptygif.emptyGifBufferLength,
-        'Cache-Control' : 'public, max-age=0'
-    });
-}
+  return emptygif.sendEmptyGif(req, res, {
+    "Content-Type": "image/gif",
+    "Content-Length": emptygif.emptyGifBufferLength,
+    "Cache-Control": "public, max-age=0"
+  });
+};
 
 module.exports = async (req, res) => {
-    const {hitType} = queryParser(req.url)
+  const { hitType } = queryParser(req.url);
 
-    if (hitType === "event") {
-        return handleEvent(req,res)
-    } else {
-        return handleTrack(req,res)
-    }
-
-}
+  if (hitType === "event") {
+    return handleEvent(req, res);
+  } else {
+    return handleTrack(req, res);
+  }
+};
--- api/reference/create/index.js
+++ api/reference/create/index.js
@@ -1,49 +1,41 @@
-import _ from "lodash"
-const requestIp = require('request-ip')
-const anonymize = require('ip-anonymize')
-import faunadb, { query as q } from 'faunadb';
+import _ from "lodash";
+const requestIp = require("request-ip");
+const anonymize = require("ip-anonymize");
+import faunadb, { query as q } from "faunadb";
 
 const { FAUNADB_SECRET: secret } = process.env;
 
 let client;
 
 if (secret) {
-    client = new faunadb.Client({ secret });
+  client = new faunadb.Client({ secret });
 }
 
 module.exports = async (req, res) => {
-    const {fingerprint, refs} = req.query
+  const { fingerprint, refs } = req.query;
 
-    if(!fingerprint || !refs) {
-        res.status(400)
-        return res.send("missing fingerprint and/or refs")
-    }
+  if (!fingerprint || !refs) {
+    res.status(400);
+    return res.send("missing fingerprint and/or refs");
+  }
 
-    let clientIP = requestIp.getClientIp(req)
-    try {
-        clientIP = anonymize(clientIP)
-    } catch (err) {
+  let clientIP = requestIp.getClientIp(req);
+  try {
+    clientIP = anonymize(clientIP);
+  } catch (err) {}
 
-    }
+  const ret = await client.query(
+    q.Create(q.Collection("references"), {
+      data: {
+        fingerprint,
+        refs: refs.split(","),
+        type: _.get(res.query, "type", "default"),
+        clientIP,
+        userAgent: req.headers && req.headers["user-agent"]
+      }
+    })
+  );
 
-    const ret = await client.query(
-        q.Create(
-            q.Collection('references'),
-            {
-                data: {
-                    fingerprint,
-                    refs: refs.split(","),
-                    type: _.get(res.query, "type", "default"),
-                    clientIP,
-                    userAgent: req.headers && req.headers['user-agent'],
-                }
-            },
-        )
-    )
-
-    console.log(ret)
-    res.send("done")
-
-
-}
-
+  console.log(ret);
+  res.send("done");
+};
