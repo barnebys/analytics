e51e64c3a8273306d21a9ab14e980957f3744f0e
Sony Sebastian <sony.sebastian@barnebys.com>
2022-07-22 14:21:17 +0200

    static files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { router, get, post } from 'microrouter';
 import { send } from 'micro';
-// import fs from require("fs").promises;
+import fsp from 'fs/promises';
 
 import createHandler from './api/reference/create';
 import fetchHandler from './api/reference/fetch';
@@ -13,15 +13,17 @@ export default router(
   get('/health-check', healthHandler),
   get('/favicon.ico', (_req, res) => send(res, 204)),
   // probably serve static files in another way later
-  // get('/bite.v1.js', async (_req, res) => {
-  //   try {
-  //     const buffer = await fs.readFile('./public/bite.v1.js');
-  //     res.setHeader('Content-Type', 'text/javascript');
-  //     send(res, 200, buffer);
-  //   } catch {
-  //     return send(res, 500);
-  //   }
-  // }),
+  get('/v1/bite.js', async (_req, res) => {
+    const jsFile = "./public/bite.v1.js"
+    let buffer = {};
+    try {
+      buffer = await fsp.readFile(jsFile).then(data => data).catch(err => {throw err});
+      res.setHeader('Content-Type', 'text/javascript');
+      return send(res, 200, buffer);
+    } catch(err) {
+      return send(res, 500, {'msg': `unable to load ${jsFile} file.`})
+    }
+  }),
   get('/r/collect', collectHandler),
   get('/r/create', createHandler),
   get('/r/fetch', fetchHandler),
